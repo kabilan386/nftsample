93f0922a1a7320e07809ef8bc278ead5518e4181
kabilan386 <kabilankabi386@gmail.com>
2023-02-09 08:50:14 +0530

    commit main branch

--- src/components/SectionSliderCategories/SectionSliderCategories.tsx
+++ src/components/SectionSliderCategories/SectionSliderCategories.tsx
@@ -31,43 +31,43 @@ const SectionSliderCategories: FC<SectionSliderCategoriesProps> = ({
   const id = useId();
   const UNIQUE_CLASS = "glidejs" + id.replace(/:/g, "_");
 
-  useEffect(() => {
-    if (!sliderRef.current) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!sliderRef.current) {
+  //     return;
+  //   }
 
-    const OPTIONS: Glide.Options = {
-      perView: 5,
-      gap: 32,
-      bound: true,
-      breakpoints: {
-        1280: {
-          perView: 4,
-        },
-        1024: {
-          gap: 20,
-          perView: 3.4,
-        },
-        768: {
-          gap: 20,
-          perView: 3,
-        },
-        640: {
-          gap: 20,
-          perView: 2.3,
-        },
-        500: {
-          gap: 20,
-          perView: 1.4,
-        },
-      },
-    };
+  //   const OPTIONS: Glide.Options = {
+  //     perView: 5,
+  //     gap: 32,
+  //     bound: true,
+  //     breakpoints: {
+  //       1280: {
+  //         perView: 4,
+  //       },
+  //       1024: {
+  //         gap: 20,
+  //         perView: 3.4,
+  //       },
+  //       768: {
+  //         gap: 20,
+  //         perView: 3,
+  //       },
+  //       640: {
+  //         gap: 20,
+  //         perView: 2.3,
+  //       },
+  //       500: {
+  //         gap: 20,
+  //         perView: 1.4,
+  //       },
+  //     },
+  //   };
 
-    let slider = new Glide(`.${UNIQUE_CLASS}`, OPTIONS);
-    slider.mount();
-    // @ts-ignore
-    return () => slider.destroy();
-  }, [sliderRef, UNIQUE_CLASS]);
+  //   let slider = new Glide(`.${UNIQUE_CLASS}`, OPTIONS);
+  //   slider.mount();
+  //   // @ts-ignore
+  //   return () => slider.destroy();
+  // }, [sliderRef, UNIQUE_CLASS]);
 
   return (
     <div className={`nc-SectionSliderCategories ${className}`}>
