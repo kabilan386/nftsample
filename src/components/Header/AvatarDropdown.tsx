7ad2317fcb60c8988d84586369f580c2b32d47a7
kabilan386 <kabilankabi386@gmail.com>
2023-02-09 08:55:50 +0530

    commit main branch

--- src/components/Header/AvatarDropdown.tsx
+++ src/components/Header/AvatarDropdown.tsx
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 import Avatar from "shared/Avatar/Avatar";
 
 export default function AvatarDropdown() {
+  const disConnect = ()=>{
+    sessionStorage.removeItem("address");
+    sessionStorage.removeItem("Connected");
+}
   return (
     <div className="AvatarDropdown">
       <Popover className="relative">
@@ -232,6 +236,7 @@ export default function AvatarDropdown() {
                     {/* ------------------ 2 --------------------- */}
                     <Link
                       to={"/"}
+                      onClick={()=> disConnect()}
                       className="flex items-center p-2 -m-3 transition duration-150 ease-in-out rounded-lg hover:bg-neutral-100 dark:hover:bg-neutral-700 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
                     >
                       <div className="flex items-center justify-center flex-shrink-0 text-neutral-500 dark:text-neutral-300">
